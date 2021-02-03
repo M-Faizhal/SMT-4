8fa1d0a84478ef82d2cc47d86fe0b5c600edf38b
M-Faizhal <68377652+M-Faizhal@users.noreply.github.com>
2021-02-03 23:37:08 +0700

    ver 1.0
    Video 1-10 React Js

---
+++ react-js/src/pages/Menu.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Tabel from "./Tabel";
+
+function Menu() {
+    const titel = "Daftar Menu Restoran"
+    const [menus, setMenu] = useState(
+        [
+            {
+                idmenu: 1, 
+                idkategori: 1, 
+                menu: "Apel Manalagi", 
+                gambar: "apel.jpg", 
+                harga: 3000
+            },
+            {
+                idmenu: 2, 
+                idkategori: 1, 
+                menu: "Pisang Raja", 
+                gambar: "pisang.jpg", 
+                harga: 5000
+            },
+            {
+                idmenu: 3, 
+                idkategori: 2, 
+                menu: "Nasi Padang", 
+                gambar: "nasipadang.jpg", 
+                harga: 20000
+            },
+            {
+                idmenu: 4, 
+                idkategori: 2, 
+                menu: "Nasi Ayam", 
+                gambar: "nasiayam.jpg", 
+                harga: 15000
+            },
+            {
+                idmenu: 5, 
+                idkategori: 3, 
+                menu: "Es Teh", 
+                gambar: "esteh.jpg", 
+                harga: 5000
+            },
+            {
+                idmenu: 66, 
+                idkategori: 3, 
+                menu: "Es Jeruk", 
+                gambar: "esjeruk.jpg", 
+                harga: 7000
+            },
+        ]
+    )
+    return (
+    <div className="App">
+        <Tabel menu = {menus} titel = {titel} />
+    </div>
+    );
+}
+
+export default Menu;
