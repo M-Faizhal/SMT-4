589adcfaeaa1f910bd63d907d7d472385ca0b121
M-Faizhal <68377652+M-Faizhal@users.noreply.github.com>
2021-03-31 21:21:35 +0700

    ver 7.0

--- project-react-restoran/react-resto/src/Axios/link.js
+++ project-react-restoran/react-resto/src/Axios/link.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const url = "http://localhost:8000/api";
-let token = "HnzO59e3F2tRXjcs3t4BqU74rRMuiCJGmyYqUv45";
+let token = sessionStorage.getItem('token');
 
 export const link = axios.create({
     baseURL : url,
